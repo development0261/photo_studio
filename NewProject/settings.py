1e7fecb21e5c18b89b7a471d5a6863b5d96d64e8
Develpoment <=>
2022-02-09 10:25:05 +0530

    updates

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -204,4 +204,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'demo.logixbuiltinfo@gmail.com'
-EMAIL_HOST_PASSWORD = '7mRL~mqU#JsM\E-F'
+EMAIL_HOST_PASSWORD = 'kvhgupucbyrtdzpr'
