06636b608920fc37d6c2159eb96592ae1985edbc
development0261 <development0261@gmail.com>
2022-07-14 17:28:45 +0530

    last

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import custom_user
-from .models import Profile, user_preference, application_data, Purchase, Tag, Product
+# from .models import Profile, user_preference, application_data, Purchase, Tag, Product
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin import ModelAdmin
 from django.contrib.admin import SimpleListFilter
@@ -67,93 +67,93 @@ class CustomUserAdmin(UserAdmin):
     search_list = ('username', 'email', 'first_name', 'last_name') 
 
 
-class AgeFilter(SimpleListFilter):
-    title = 'age'
-    parameter_name = 'age'
+# class AgeFilter(SimpleListFilter):
+#     title = 'age'
+#     parameter_name = 'age'
 
-    def lookups(self, request, model_admin):
-        return (
-            ('20 - 30', '20 - 30'),
-            ('30 - 40', '30 - 40'),
-            ('>40', 'Greater than 40'),
-        )
+#     def lookups(self, request, model_admin):
+#         return (
+#             ('20 - 30', '20 - 30'),
+#             ('30 - 40', '30 - 40'),
+#             ('>40', 'Greater than 40'),
+#         )
 
-    def queryset(self, request, queryset):
-        if self.value() == '20 - 30':
-            return queryset.filter(dob__gte=datetime.now() - timedelta(days=(365*30)), dob__lt=datetime.now() - timedelta(days=(365*20)))
-        if self.value() == '30 - 40':
-            return queryset.filter(dob__gte=datetime.now() - timedelta(days=(365*40)), dob__lt=datetime.now() - timedelta(days=(365*30)))
-        if self.value() == '>40':
-            return queryset.filter(dob__lte=datetime.now() - timedelta(days=(365*40)))
-        if self.value():
-            return queryset
-
-
-class PassowordUpdateFilter(SimpleListFilter):
-    title = 'Last Password Updated'
-    parameter_name = 'pass_update'
-
-    def lookups(self, request, model_admin):
-        return (
-            ('Today', 'Today'),
-            ('Past 7 days', 'Past 7 days'),
-            ('This month', 'This month'),
-            ('This year', 'This year'),
-        )
-
-    def queryset(self, request, queryset):
-        if self.value() == 'Today':
-            return queryset.filter(pass_update__exact=datetime.now())
-        if self.value() == 'Past 7 days':
-            return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=7), pass_update__lte=datetime.now())
-        if self.value() == 'This month':
-            return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=30), pass_update__lte=datetime.now())
-        if self.value() == 'This year':
-            return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=365), pass_update__lte=datetime.now())
-        if self.value():
-            return queryset
+#     def queryset(self, request, queryset):
+#         if self.value() == '20 - 30':
+#             return queryset.filter(dob__gte=datetime.now() - timedelta(days=(365*30)), dob__lt=datetime.now() - timedelta(days=(365*20)))
+#         if self.value() == '30 - 40':
+#             return queryset.filter(dob__gte=datetime.now() - timedelta(days=(365*40)), dob__lt=datetime.now() - timedelta(days=(365*30)))
+#         if self.value() == '>40':
+#             return queryset.filter(dob__lte=datetime.now() - timedelta(days=(365*40)))
+#         if self.value():
+#             return queryset
 
 
-class CustomProfile(admin.ModelAdmin):
-    def Profile_Image(self, object):
-        if object.profile_image:
-            return format_html('<img src="{}" width="40" style="border-radius:50px">'.format(object.profile_image.url))
+# class PassowordUpdateFilter(SimpleListFilter):
+#     title = 'Last Password Updated'
+#     parameter_name = 'pass_update'
 
-    list_display = ('username', 'name', 'Profile_Image')
-    list_display_links = ('username',)
-    list_filter = ("gender", "country", AgeFilter, PassowordUpdateFilter)
-    search_fields = ("avatar", "bitmoji", "city", "country", "created_at", "dob", "fb", "gender", "insta", "lat",
-                     "long", "mobile", "name", "pass_forgot", "pass_update", "profile_image", "snap", "uid", "updated_at", "website")
+#     def lookups(self, request, model_admin):
+#         return (
+#             ('Today', 'Today'),
+#             ('Past 7 days', 'Past 7 days'),
+#             ('This month', 'This month'),
+#             ('This year', 'This year'),
+#         )
 
-
-class CustomDetails(admin.ModelAdmin):
-    list_display = ("username", 'Language')
-    search_fields = ("Language", "app_theme", "appearance_mode", "digital_riyals_rewards", "export_quality", "most_used_fonts", "udid", "user_custom_colors",
-                     "user_recent_text", "user_stared_Textart", "user_stared_backgrounds", "user_stared_colors", "user_stared_fonts", "user_stared_stickers", "user_stared_templates")
-
-
-class CustomApps(admin.ModelAdmin):
-    list_display = ("username", 'UID')
-    search_fields = ("Carrier", "Device_Model", "Device_Storage", "Grace_Period", "Latest_Geolocation",
-                     "Purchased_product", "Remaining_grace_period_days", "Total_time_spent", "UID", "aid", "iOS", "inApp_Products")
-
-
-class CustomPurchase(admin.ModelAdmin):
-    search_fields = ["pstatus", ]
+#     def queryset(self, request, queryset):
+#         if self.value() == 'Today':
+#             return queryset.filter(pass_update__exact=datetime.now())
+#         if self.value() == 'Past 7 days':
+#             return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=7), pass_update__lte=datetime.now())
+#         if self.value() == 'This month':
+#             return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=30), pass_update__lte=datetime.now())
+#         if self.value() == 'This year':
+#             return queryset.filter(pass_update__gte=datetime.now() - timedelta(days=365), pass_update__lte=datetime.now())
+#         if self.value():
+#             return queryset
 
 
-class CustomTag(admin.ModelAdmin):
-    search_fields = ["tag", ]
+# class CustomProfile(admin.ModelAdmin):
+#     def Profile_Image(self, object):
+#         if object.profile_image:
+#             return format_html('<img src="{}" width="40" style="border-radius:50px">'.format(object.profile_image.url))
 
-class CustomProduct(admin.ModelAdmin):
-    search_fields = ["annaulSub", "annaulSubProd", "localeId", "monthlySub", "monthlySubProd", "product", "productID", "productPromo", "promoPrice"]
+#     list_display = ('username', 'name', 'Profile_Image')
+#     list_display_links = ('username',)
+#     list_filter = ("gender", "country", AgeFilter, PassowordUpdateFilter)
+#     search_fields = ("avatar", "bitmoji", "city", "country", "created_at", "dob", "fb", "gender", "insta", "lat",
+#                      "long", "mobile", "name", "pass_forgot", "pass_update", "profile_image", "snap", "uid", "updated_at", "website")
+
+
+# class CustomDetails(admin.ModelAdmin):
+#     list_display = ("username", 'Language')
+#     search_fields = ("Language", "app_theme", "appearance_mode", "digital_riyals_rewards", "export_quality", "most_used_fonts", "udid", "user_custom_colors",
+#                      "user_recent_text", "user_stared_Textart", "user_stared_backgrounds", "user_stared_colors", "user_stared_fonts", "user_stared_stickers", "user_stared_templates")
+
+
+# class CustomApps(admin.ModelAdmin):
+#     list_display = ("username", 'UID')
+#     search_fields = ("Carrier", "Device_Model", "Device_Storage", "Grace_Period", "Latest_Geolocation",
+#                      "Purchased_product", "Remaining_grace_period_days", "Total_time_spent", "UID", "aid", "iOS", "inApp_Products")
+
+
+# class CustomPurchase(admin.ModelAdmin):
+#     search_fields = ["pstatus", ]
+
+
+# class CustomTag(admin.ModelAdmin):
+#     search_fields = ["tag", ]
+
+# class CustomProduct(admin.ModelAdmin):
+#     search_fields = ["annaulSub", "annaulSubProd", "localeId", "monthlySub", "monthlySubProd", "product", "productID", "productPromo", "promoPrice"]
 
 
 admin.site.register(custom_user,CustomUserAdmin)
-admin.site.register(Profile, CustomProfile)
-admin.site.register(user_preference, CustomDetails)
-admin.site.register(application_data, CustomApps)
-admin.site.register(Purchase, CustomPurchase)
-admin.site.register(Tag, CustomTag)
-admin.site.register(Product, CustomProduct)
+# admin.site.register(Profile, CustomProfile)
+# admin.site.register(user_preference, CustomDetails)
+# admin.site.register(application_data, CustomApps)
+# admin.site.register(Purchase, CustomPurchase)
+# admin.site.register(Tag, CustomTag)
+# admin.site.register(Product, CustomProduct)
 
--- home/models.py
+++ home/models.py
@@ -18,195 +18,195 @@ class custom_user(AbstractUser):
         unique_together = ('email',)
 
 
-class Profile(models.Model):
-    uid = models.AutoField(primary_key=True, auto_created=True)
-    username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, null=True, blank=True)
-    # email = models.CharField(max_length=50, null=True, blank=True, unique=True)
-    mobile = models.CharField(max_length=20, null=True, blank=True)
-    GENDER_CHOICES = (
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-        ('Other', 'Other'),
-    )
-    gender = models.CharField(
-        max_length=7, choices=GENDER_CHOICES, null=True, blank=True)
-    profile_image = models.ImageField(null=True, blank=True, upload_to='')
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    pass_update = models.DateField(blank=True, null=True)
-    pass_forgot = models.DateField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-    dob = models.DateField(null=True, blank=True)
-    city = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=20, null=True, blank=True)
-    lat = models.FloatField(max_length=30, null=True, blank=True)
-    long = models.FloatField(max_length=30, null=True, blank=True)
-    snap = models.CharField(max_length=30, null=True, blank=True)
-    fb = models.CharField(max_length=30, null=True, blank=True)
-    insta = models.CharField(max_length=30, null=True, blank=True)
-    website = models.CharField(max_length=100, null=True, blank=True)
-    avatar = models.ImageField(null=True, blank=True)
-    bitmoji = models.ImageField(null=True, blank=True)
-    delete_account = models.BooleanField(default=False)
-    expiration_date = models.DateTimeField(null=True, blank=True)
-    count_for_forgot_pass = models.PositiveSmallIntegerField(default=0)
-    time_for_forgot_pass = models.DateTimeField(null=True, blank=True)
+# class Profile(models.Model):
+#     uid = models.AutoField(primary_key=True, auto_created=True)
+#     username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=50, null=True, blank=True)
+#     # email = models.CharField(max_length=50, null=True, blank=True, unique=True)
+#     mobile = models.CharField(max_length=20, null=True, blank=True)
+#     GENDER_CHOICES = (
+#         ('Male', 'Male'),
+#         ('Female', 'Female'),
+#         ('Other', 'Other'),
+#     )
+#     gender = models.CharField(
+#         max_length=7, choices=GENDER_CHOICES, null=True, blank=True)
+#     profile_image = models.ImageField(null=True, blank=True, upload_to='')
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+#     pass_update = models.DateField(blank=True, null=True)
+#     pass_forgot = models.DateField(blank=True, null=True)
+#     updated_at = models.DateTimeField(blank=True, null=True)
+#     dob = models.DateField(null=True, blank=True)
+#     city = models.CharField(max_length=20, null=True, blank=True)
+#     country = models.CharField(max_length=20, null=True, blank=True)
+#     lat = models.FloatField(max_length=30, null=True, blank=True)
+#     long = models.FloatField(max_length=30, null=True, blank=True)
+#     snap = models.CharField(max_length=30, null=True, blank=True)
+#     fb = models.CharField(max_length=30, null=True, blank=True)
+#     insta = models.CharField(max_length=30, null=True, blank=True)
+#     website = models.CharField(max_length=100, null=True, blank=True)
+#     avatar = models.ImageField(null=True, blank=True)
+#     bitmoji = models.ImageField(null=True, blank=True)
+#     delete_account = models.BooleanField(default=False)
+#     expiration_date = models.DateTimeField(null=True, blank=True)
+#     count_for_forgot_pass = models.PositiveSmallIntegerField(default=0)
+#     time_for_forgot_pass = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.username} - {self.name}"
+#     def __str__(self):
+#         return f"{self.username} - {self.name}"
 
-    class Meta:
-        verbose_name_plural = "Profile"
-
-
-class user_preference(models.Model):
-    udid = models.AutoField(primary_key=True, auto_created=True)
-    username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
-    signature = models.ImageField(null=True)
-    quality_choices = (
-        ('Low', 'Low'),
-        ('Medium', 'Medium'),
-        ('High', 'High'),
-    )
-    export_quality = models.CharField(
-        max_length=7, choices=quality_choices)  # (list) high, medium, low
-    Language = models.CharField(max_length=20)  # (string)
-    user_stared_templates = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    user_stared_backgrounds = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    user_stared_stickers = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    user_stared_Textart = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    user_stared_colors = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    user_stared_fonts = ArrayField(models.CharField(
-        max_length=50), blank=True)  # (array)
-    most_used_fonts = ArrayField(models.CharField(
-        max_length=50), blank=True)  # (array)
-    user_custom_colors = ArrayField(
-        models.CharField(max_length=50), blank=True)  # (array)
-    instagram_follower = models.BooleanField(default=False)  # (boolean)
-    grid_snapping = models.BooleanField(default=False)  # (boolean)
-    user_recent_text = models.CharField(max_length=255)  # (string)
-    appearance_choices = (
-        ('Light', 'Light'),
-        ('Dark', 'Dark'),
-        ('Auto', 'Auto'),
-    )
-    appearance_mode = models.CharField(
-        max_length=7, choices=appearance_choices)  # (list): dark, light, auto
-    enable_iCloud_backup = models.BooleanField(default=False)  # (boolean)
-    save_projects_automatically = models.BooleanField(
-        default=False)  # (boolean)
-    save_projects_on_export = models.BooleanField(default=False)  # (boolean)
-    notifications_permission = models.BooleanField(default=False)  # (boolean)
-    inApp_notifications_permission = models.BooleanField(
-        default=False)  # (boolean)
-    photo_library_permission = models.BooleanField(default=False)  # (boolean)
-    digital_riyals_rewards = models.IntegerField()  # (Number)
-    enable_touch = models.BooleanField(default=False)  # (boolean)
-    app_choices = (
-        ('Option1', 'Option1'),
-        ('Option2', 'Option2'),
-        ('Option3', 'Option3'),
-    )
-    app_theme = models.CharField(max_length=7, choices=app_choices)  # (list)
-    always_crop = models.BooleanField(default=False)  # (boolean)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.username} - {self.udid}"
-
-    class Meta:
-        verbose_name_plural = "user_preference"
+#     class Meta:
+#         verbose_name_plural = "Profile"
 
 
-class application_data(models.Model):
-    aid = models.AutoField(primary_key=True, auto_created=True)
-    UID = models.CharField(unique=True, max_length=200)
-    username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
-    inApp_Products = models.CharField(max_length=100)
-    Purchase_date = models.DateTimeField(blank=True, null=True)
-    Purchased_product = models.CharField(max_length=100)
-    Device_Model = models.CharField(max_length=50)
-    operating_system = models.CharField(max_length=100)
-    Device_Storage = models.CharField(max_length=10)
-    Lunch_count = models.IntegerField()
-    Push_Notification_Status = models.BooleanField(default=False)
-    Library_permission_Status = models.BooleanField(default=False)
-    latitude = models.FloatField(blank=True, null=True,max_length=30)
-    longitude = models.FloatField(blank=True, null=True,max_length=30)
-    Carrier = models.CharField(max_length=100)
-    App_Last_Opened = models.DateTimeField(blank=True, null=True)
-    Purchase_attempts = models.IntegerField()
-    Grace_Period = models.CharField(max_length=100)
-    Remaining_grace_period_days = models.IntegerField()
-    Number_of_projects = models.IntegerField()
-    Total_time_spent = models.CharField(max_length=10)
-    total_ads_served = models.IntegerField()
-    Registered_user = models.BooleanField(default=False)
-    Push_Notification_token = models.CharField(
-        max_length=255, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+# class user_preference(models.Model):
+#     udid = models.AutoField(primary_key=True, auto_created=True)
+#     username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
+#     signature = models.ImageField(null=True)
+#     quality_choices = (
+#         ('Low', 'Low'),
+#         ('Medium', 'Medium'),
+#         ('High', 'High'),
+#     )
+#     export_quality = models.CharField(
+#         max_length=7, choices=quality_choices)  # (list) high, medium, low
+#     Language = models.CharField(max_length=20)  # (string)
+#     user_stared_templates = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     user_stared_backgrounds = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     user_stared_stickers = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     user_stared_Textart = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     user_stared_colors = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     user_stared_fonts = ArrayField(models.CharField(
+#         max_length=50), blank=True)  # (array)
+#     most_used_fonts = ArrayField(models.CharField(
+#         max_length=50), blank=True)  # (array)
+#     user_custom_colors = ArrayField(
+#         models.CharField(max_length=50), blank=True)  # (array)
+#     instagram_follower = models.BooleanField(default=False)  # (boolean)
+#     grid_snapping = models.BooleanField(default=False)  # (boolean)
+#     user_recent_text = models.CharField(max_length=255)  # (string)
+#     appearance_choices = (
+#         ('Light', 'Light'),
+#         ('Dark', 'Dark'),
+#         ('Auto', 'Auto'),
+#     )
+#     appearance_mode = models.CharField(
+#         max_length=7, choices=appearance_choices)  # (list): dark, light, auto
+#     enable_iCloud_backup = models.BooleanField(default=False)  # (boolean)
+#     save_projects_automatically = models.BooleanField(
+#         default=False)  # (boolean)
+#     save_projects_on_export = models.BooleanField(default=False)  # (boolean)
+#     notifications_permission = models.BooleanField(default=False)  # (boolean)
+#     inApp_notifications_permission = models.BooleanField(
+#         default=False)  # (boolean)
+#     photo_library_permission = models.BooleanField(default=False)  # (boolean)
+#     digital_riyals_rewards = models.IntegerField()  # (Number)
+#     enable_touch = models.BooleanField(default=False)  # (boolean)
+#     app_choices = (
+#         ('Option1', 'Option1'),
+#         ('Option2', 'Option2'),
+#         ('Option3', 'Option3'),
+#     )
+#     app_theme = models.CharField(max_length=7, choices=app_choices)  # (list)
+#     always_crop = models.BooleanField(default=False)  # (boolean)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.username} - {self.UID}"
+#     def __str__(self):
+#         return f"{self.username} - {self.udid}"
 
-    class Meta:
-        verbose_name_plural = "Application data"
-
-
-class Product(models.Model):
-    PID = models.AutoField(primary_key=True, auto_created=True)
-    productID = models.CharField(max_length=255, unique=True)
-    product = models.CharField(max_length=255, unique=True)
-    productPromo = models.CharField(max_length=255, null=True, blank=True)
-    promoPrice = models.FloatField(max_length=255, null=True, blank=True)
-    annaulSubProd = models.CharField(max_length=255, null=True, blank=True)
-    annaulSub = models.CharField(max_length=255, null=True, blank=True)
-    monthlySubProd = models.CharField(max_length=255, null=True, blank=True)
-    monthlySub = models.CharField(max_length=255, null=True, blank=True)
-    localeId = models.IntegerField(null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.product}"
-
-    class Meta:
-        verbose_name_plural = "Product"
-
-
-class Purchase(models.Model):
-    pid = models.AutoField(primary_key=True, auto_created=True)
-    username = models.ForeignKey(custom_user, on_delete=models.CASCADE, null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
-    pstatus = models.CharField(max_length=20)
-    auto_renew_status = models.BooleanField(default=False)
-    is_in_billing_retry_period = models.BooleanField(default=False)
-    is_in_intro_offer_period = models.BooleanField(default=False)
-    is_trial_period = models.BooleanField(default=False)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
-    subscription_type = models.CharField(max_length=255, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
-    def __str__(self):
-        return f"{self.username} - {self.pstatus}"
-
-    class Meta:
-        verbose_name_plural = "Purchase"
+#     class Meta:
+#         verbose_name_plural = "user_preference"
 
 
-class Tag(models.Model):
-    username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
-    tag = ArrayField(models.CharField(max_length=50), null=True, max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+# class application_data(models.Model):
+#     aid = models.AutoField(primary_key=True, auto_created=True)
+#     UID = models.CharField(unique=True, max_length=200)
+#     username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
+#     inApp_Products = models.CharField(max_length=100)
+#     Purchase_date = models.DateTimeField(blank=True, null=True)
+#     Purchased_product = models.CharField(max_length=100)
+#     Device_Model = models.CharField(max_length=50)
+#     operating_system = models.CharField(max_length=100)
+#     Device_Storage = models.CharField(max_length=10)
+#     Lunch_count = models.IntegerField()
+#     Push_Notification_Status = models.BooleanField(default=False)
+#     Library_permission_Status = models.BooleanField(default=False)
+#     latitude = models.FloatField(blank=True, null=True,max_length=30)
+#     longitude = models.FloatField(blank=True, null=True,max_length=30)
+#     Carrier = models.CharField(max_length=100)
+#     App_Last_Opened = models.DateTimeField(blank=True, null=True)
+#     Purchase_attempts = models.IntegerField()
+#     Grace_Period = models.CharField(max_length=100)
+#     Remaining_grace_period_days = models.IntegerField()
+#     Number_of_projects = models.IntegerField()
+#     Total_time_spent = models.CharField(max_length=10)
+#     total_ads_served = models.IntegerField()
+#     Registered_user = models.BooleanField(default=False)
+#     Push_Notification_token = models.CharField(
+#         max_length=255, null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.username} - {self.tag}"
+#     def __str__(self):
+#         return f"{self.username} - {self.UID}"
 
-    class Meta:
-        verbose_name_plural = "Tag"
+#     class Meta:
+#         verbose_name_plural = "Application data"
+
+
+# class Product(models.Model):
+#     PID = models.AutoField(primary_key=True, auto_created=True)
+#     productID = models.CharField(max_length=255, unique=True)
+#     product = models.CharField(max_length=255, unique=True)
+#     productPromo = models.CharField(max_length=255, null=True, blank=True)
+#     promoPrice = models.FloatField(max_length=255, null=True, blank=True)
+#     annaulSubProd = models.CharField(max_length=255, null=True, blank=True)
+#     annaulSub = models.CharField(max_length=255, null=True, blank=True)
+#     monthlySubProd = models.CharField(max_length=255, null=True, blank=True)
+#     monthlySub = models.CharField(max_length=255, null=True, blank=True)
+#     localeId = models.IntegerField(null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return f"{self.product}"
+
+#     class Meta:
+#         verbose_name_plural = "Product"
+
+
+# class Purchase(models.Model):
+#     pid = models.AutoField(primary_key=True, auto_created=True)
+#     username = models.ForeignKey(custom_user, on_delete=models.CASCADE, null=True, blank=True)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+#     pstatus = models.CharField(max_length=20)
+#     auto_renew_status = models.BooleanField(default=False)
+#     is_in_billing_retry_period = models.BooleanField(default=False)
+#     is_in_intro_offer_period = models.BooleanField(default=False)
+#     is_trial_period = models.BooleanField(default=False)
+#     start_date = models.DateField(blank=True, null=True)
+#     end_date = models.DateField(blank=True, null=True)
+#     subscription_type = models.CharField(max_length=255, blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return f"{self.username} - {self.pstatus}"
+
+#     class Meta:
+#         verbose_name_plural = "Purchase"
+
+
+# class Tag(models.Model):
+#     username = models.OneToOneField(custom_user, on_delete=models.CASCADE)
+#     tag = ArrayField(models.CharField(max_length=50), null=True, max_length=50)
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return f"{self.username} - {self.tag}"
+
+#     class Meta:
+#         verbose_name_plural = "Tag"
 
